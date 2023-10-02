0a813d226a3f39d06b100d08aed58b23adb8f8fc
Michael Thomas <mcthoma4@CougarNet.UH.EDU>
2023-10-02 18:48:19 -0500

    added sound

--- Assets/playerScripts/attackScript.cs
+++ Assets/playerScripts/attackScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class attackScript : MonoBehaviour
 {
@@ -12,9 +13,30 @@ public class attackScript : MonoBehaviour
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
     public int attackDamage = 5;
-   
+
+    public float dirX;
+    /*public float X;
+    public float Y;*/
+
+    
+
     void Update()
     {
+        
+        
+        dirX = Input.GetAxisRaw("Horizontal");
+        /*if(dirX > 0)
+        {
+            attackPoint.position = new Vector3(1.06f, 1.81f, 0.02000046f);
+        }
+        else if( dirX < 0)
+        {
+            attackPoint.position = new Vector3(-1.06f, 1.81f, 0.02000046f);
+
+        }*/
+
+        
+
         if (Input.GetKeyDown(KeyCode.Mouse0) && canAttack)
         {   
             attack();
@@ -34,10 +56,12 @@ public class attackScript : MonoBehaviour
         animator.SetTrigger("Attack");
         
         Collider2D[] hitEnemies  = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        //Collider2D[] hitEnemies  = Physics2D.OverlapBoxAll(attackPoint.position, new Vector2(X,Y), enemyLayers);
 
-        foreach(Collider2D enemy in hitEnemies)
+
+        foreach (Collider2D enemy in hitEnemies)
         {
-            //Debug.Log("We hit" + enemy.name);
+            
           enemy.GetComponent<Bishop_Crab>().TakeDamage(attackDamage);
         }
         
@@ -58,5 +82,8 @@ public class attackScript : MonoBehaviour
             return;
         }
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        //Gizmos.DrawWireCube(attackPoint.position, new Vector3(X, Y, 1));
     }
+
+    
 }
