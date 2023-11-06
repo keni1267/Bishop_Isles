3460780193219cd4cc84d2d9f8990a2f6f7895e2
BPeeTee <98679709+BPeeTee@users.noreply.github.com>
2023-11-06 01:11:14 -0600

    Tutorial Complete except some Dialogue bugs

--- Assets/CompleteTutorialScript.cs
+++ Assets/CompleteTutorialScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class textScript : MonoBehaviour {
+public class CompleteTutorialScript : MonoBehaviour {
     public Text diatext;
     public GameObject diaPanel;
     public GameObject button;
@@ -15,46 +15,39 @@ public class textScript : MonoBehaviour {
     private bool playerIsClose = false;
     private bool inConversation = false; // Added a flag for conversation state
     public bool firstTime = true;
-    void Update()
-    {
-        if (firstTime == true)
-        {
-            this.enabled= false;
+    void Update() {
+        if (firstTime == true) {
+            this.enabled = false;
             StartCoroutine(ExecuteAfterDelay(16f));
-            firstTime= false;
+            firstTime = false;
 
         }
 
-        if (playerIsClose && !inConversation)
-        {
+        if (playerIsClose && !inConversation) {
             StartConversation();
         }
 
-        if (index == 4)
-        {
+        if (index == 1) {
             this.enabled = false;
             //firstTime = true;
         }
 
     }
 
-    IEnumerator ExecuteAfterDelay(float delay)
-    {
+    IEnumerator ExecuteAfterDelay(float delay) {
         yield return new WaitForSeconds(delay);
         this.enabled = true;
         // Code to execute after the delay
         Debug.Log("10 seconds have passed!");
     }
 
-    void StartConversation()
-    {
+    void StartConversation() {
         inConversation = true;
         diaPanel.SetActive(true);
         StartCoroutine(Typing());
     }
 
-    public void zeroText()
-    {
+    public void zeroText() {
         diatext.text = "";
         index = 0;
         diaPanel.SetActive(false);
@@ -62,48 +55,38 @@ public class textScript : MonoBehaviour {
         //this.enabled = false;
     }
 
-    IEnumerator Typing()
-    {
-        foreach (char letter in dialogue[index].ToCharArray())
-        {
+    IEnumerator Typing() {
+        foreach (char letter in dialogue[index].ToCharArray()) {
             diatext.text += letter;
             yield return new WaitForSeconds(wordSpeed);
         }
         button.SetActive(true);
     }
 
-    public void NextLine()
-    {
+    public void NextLine() {
         button.SetActive(false);
-        if (index < dialogue.Length - 1)
-        {
+        if (index < dialogue.Length - 1) {
             index++;
             diatext.text = "";
             StartCoroutine(Typing());
-        }
-        else
-        {
+        } else {
             zeroText();
 
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
             playerIsClose = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
             playerIsClose = false;
             zeroText();
         }
     }
-    
+
 }
 
