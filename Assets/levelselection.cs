d5acd6795429eb9a9dceaf922788134f37759db8
Michael Thomas <mcthoma4@CougarNet.UH.EDU>
2023-11-24 15:21:40 -0600

    added level selection/transition logic

---
+++ Assets/levelselection.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class levelselection : MonoBehaviour
+{
+
+    public Button[] lvlButtons;
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2) - 1;
+        Debug.Log(levelAt);
+        for(int i = 0;i < lvlButtons.Length;i++)
+        {
+            if(i + 2 > levelAt)
+            {
+                lvlButtons[i].interactable = false;
+            }
+        }
+        
+    }
+
+    
+}
