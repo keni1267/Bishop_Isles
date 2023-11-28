b109f882297a891cc64b4cb098f51f2681a08732
Kenia Benitez <keniab459@gmail.com>
2023-11-28 15:42:27 -0600

    health UI stuff

--- Assets/player_health.cs
+++ Assets/player_health.cs
@@ -13,6 +13,9 @@ public class player_health : MonoBehaviour
     public float health;
     public float max_health;
     public Image HealthBar;
+    public Image Lobster;
+    public Image Crab;
+    public Image Krill;
     private bool IsAlive;
     public Healthpickup pickup;
 
@@ -23,12 +26,27 @@ public class player_health : MonoBehaviour
     void Start()
     {
         max_health = health;
+        Lobster.gameObject.SetActive(true);
+        Crab.gameObject.SetActive(false);
+        Krill.gameObject.SetActive(false);
     }
 
     void Update()
     {
         HealthBar.fillAmount = Mathf.Clamp(health / max_health, 0 ,1);
+        if(health < 70 && health > 40){
+            Lobster.gameObject.SetActive(false);
+            Crab.gameObject.SetActive(true);
+        }
+        if(health <= 40){
+            //Lobster.gameObject.SetActive(false);
+            Crab.gameObject.SetActive(false);
+            Krill.gameObject.SetActive(true);
+        }
+  
+
     }
+
     public void Damage(int amount){
         if(health > 0){
             health -= amount;                
@@ -68,5 +86,7 @@ public class player_health : MonoBehaviour
             
         }
     }
+
+
 
 }
