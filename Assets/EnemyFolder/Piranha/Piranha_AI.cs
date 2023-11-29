9d32ddbeb239d384c6cb485d6eff75855cc73f8a
Kenia Benitez <keniab459@gmail.com>
2023-11-29 16:45:36 -0600

    piranha flips when walking

--- Assets/EnemyFolder/Piranha/Piranha_AI.cs
+++ Assets/EnemyFolder/Piranha/Piranha_AI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Piranha_AI : MonoBehaviour
 {
     #region Public Variables
@@ -9,27 +10,21 @@ public class Piranha_AI : MonoBehaviour
     public float lineOfSite;
     public float attackRange;
     public float timer; //attack cooldown
-    public player_health playerHealth;   //accesing script needed for player damage
+    public player_health playerHealth;   //accessing script needed for player damage
     public int damage;
-
+    public Transform[] waypoints;
     #endregion
-    
 
     #region Private Variables
     private int waypointIndex = 0;
-    private Transform player;
-    [SerializeField]
-    private Transform[] waypoints;
     private Animator anim;
     private bool attackMode;
     private bool cooling;
     private float intTimer;
     private bool hasAttacked = false; //so it only gets damage once
-
     #endregion
 
-
-   void Awake()
+    void Awake()
     {
         intTimer = timer;
         anim = GetComponent<Animator>();
@@ -37,78 +32,94 @@ public class Piranha_AI : MonoBehaviour
 
     void Start()
     {
-        transform.position = waypoints[waypointIndex].transform.position;   //position between waypoints
-        player = GameObject.FindGameObjectWithTag("Player").transform;  //fisherman has tag player
-        playerHealth = player.GetComponent<player_health>(); //gets player health from player object
-      
+        transform.position = waypoints[waypointIndex].position; //position between waypoints
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<player_health>(); //gets player health from player object
     }
 
     void Update()
     {
-        float distance_from_player = Vector2.Distance(player.position,transform.position);  //checks distance from enemy and fisherman
+        float distance_from_player = Vector2.Distance(playerHealth.transform.position, transform.position); //checks distance from enemy and fisherman
         if (distance_from_player < lineOfSite && distance_from_player > attackRange)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.position,attack_speed*Time.deltaTime);  //moves towards player
-            if(hasAttacked == false){
+            transform.position = Vector2.MoveTowards(transform.position, playerHealth.transform.position, attack_speed * Time.deltaTime); //moves towards player
+            if (hasAttacked == false)
+            {
                 Attack();
                 hasAttacked = true;
             }
         }
-        else if(distance_from_player <= attackRange && cooling == false && hasAttacked == false){
+        else if (distance_from_player <= attackRange && cooling == false && hasAttacked == false)
+        {
             Attack();
             hasAttacked = true;
         }
-       if(cooling){
+        if (cooling)
+        {
             Cooldown();
-            anim.SetBool("Attack",false);
+            anim.SetBool("Attack", false);
         }
-        else{
-            Patrol();     //stable move
+        else
+        {
+            Patrol(); //stable move
         }
     }
 
-    private void OnDrawGizmosSelected()    //creates line of site
+    private void OnDrawGizmosSelected() //creates line of site
     {
-        Gizmos.color = Color.blue; 
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, lineOfSite);
         Gizmos.DrawWireSphere(transform.position, attackRange);
-
     }
-    private void Patrol(){
-        Transform wp = waypoints[waypointIndex];
-        if(Vector2.Distance(transform.position, wp.position) < 0.01f){
+
+    private void Patrol()
+    {
+        if (waypoints.Length == 0)
+        {
+            Debug.LogWarning("No waypoints assigned for patrolling.");
+            return;
+        }
+
+        Transform targetWaypoint = waypoints[waypointIndex];
+        float horizontalMove = Mathf.Sign(targetWaypoint.position.x - transform.position.x);
+
+        // Flip the enemy based on the movement direction
+        if (horizontalMove > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (horizontalMove < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, targetWaypoint.position) < 0.1f)
+        {
             waypointIndex = (waypointIndex + 1) % waypoints.Length;
-        }
-        else{
-            transform.position = Vector2.MoveTowards(transform.position,
-               wp.position, speed* Time.deltaTime);
+            hasAttacked = false; //reset attack status when changing waypoint
         }
     }
 
-    void Attack(){
-        timer = intTimer;       //reset timer when player in attack range
+    void Attack()
+    {
+        timer = intTimer; //reset timer when player in attack range
         attackMode = true;
-       // anim.SetBool("Attack",true);
-        GetComponent<Animator>().SetTrigger("Attack"); // same as anim.SetBool("Attack", true) but it doesnt glitch as much
+        anim.SetTrigger("Attack"); // same as anim.SetBool("Attack", true) but it doesn't glitch as much
 
         if (attackMode && playerHealth != null)
-           playerHealth.Damage(damage);
-        
-    
+            playerHealth.Damage(damage);
     }
-    void Cooldown(){
+
+    void Cooldown()
+    {
         timer -= Time.deltaTime;
-        if(timer <= 0 && cooling && attackMode){
+        if (timer <= 0 && cooling && attackMode)
+        {
             cooling = false;
             timer = intTimer;
             hasAttacked = false;
         }
     }
 
-   public void TriggerCooling(){
+    public void TriggerCooling()
+    {
         cooling = true;
     }
 }
-
-//https://www.youtube.com/watch?v=lHLZxd0O6XY
-//https://www.youtube.com/watch?v=waj6i9cQ6rM
