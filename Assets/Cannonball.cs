0d260c7ab959ff9f5896f8a76b15ad07ce69191e
Kenia Benitez <keniab459@gmail.com>
2023-11-30 09:36:52 -0600

    archerfishAI

--- Assets/Cannonball.cs
+++ Assets/Cannonball.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class Cannonball : MonoBehaviour
 {
     public float speed = 5.0f;
-    GameObject target;
-    
+    public float fallSpeed = 0.005f; // Adjust this value for a slower fall
+
     Rigidbody2D ball;
 
     void Start()
     {
         ball = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player");
-        Vector2 moveDir = (target.transform.position - transform.position).normalized*speed;
-        ball.velocity = new Vector2(moveDir.x,moveDir.y);
-        //Destroy(this.gameObject,2);
+
+        // Apply an initial velocity with a higher horizontal component
+        Vector2 initialVelocity = new Vector2(1.0f, 0.1f) * speed; // Adjust the values for the desired trajectory
+        ball.velocity = initialVelocity;
+       // Destroy(gameObject,4f);
+        // Uncomment the line below if you want to apply an initial force instead
+        // ball.AddForce(initialVelocity, ForceMode2D.Impulse);
     }
 
+    void Update()
+    {
+        // Apply gravity to make the bullet fall
+        ball.velocity += Vector2.down * fallSpeed * Time.deltaTime;
+    }
 }
